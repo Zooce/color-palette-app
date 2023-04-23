9bd4a9a3074179c25d91a727752b042301e16ded
Zooce <zooce@hey.com>
2023-04-23 01:12:27 -0700

    forgot to commit...
    Lots of stuff in this commit. Everything is working nicely!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- src/routes/index.svelte -->
+<script>
+  import ColorControls from '$lib/ColorControls.svelte';
+  let colorMode = 'RGB';
+
+  /** @type {number[]} */
+  let colorIds = [1, 2, 3];
+  let nextId = 4;
+
+  function addColor() {
+    colorIds = [...colorIds, nextId];
+    nextId += 1;
+  }
+
+  function removeColor(/** @type {Event} */ event) {
+    // @ts-ignore
+    colorIds.splice(event.detail.index, 1);
+    colorIds = colorIds;
+    console.log(colorIds);
+  }
+
+  let showControls = true;
+  let toggleText = 'Hide Controls';
+
+  function toggleControls() {
+    if (showControls) {
+      toggleText = 'Show Controls';
+    } else {
+      toggleText = 'Hide Controls';
+    }
+    showControls = !showControls;
+  }
+</script>
+
+<style>
+  * {
+    font-family:'Courier New', Courier, monospace;
+  }
+  .color-palette {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+  .color-container {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .common-controls {
+    display: flex;
+    justify-content: left;
+    gap: 1rem;
+    margin-bottom: 10px;
+  }
+</style>
+
+<div class=common-controls>
+  <select bind:value={colorMode}>
+    <option value=RGB>RGB</option>
+    <option value=HSB>HSB</option>
+  </select>
+
+  <button on:click={addColor}>Add Color</button>
+
+  <button on:click={toggleControls}>{toggleText}</button>
+</div>
+
+<div class=color-palette>
+  {#each colorIds as colorId, index (colorId)}
+  <div class=color-container>
+    <ColorControls bind:mode={colorMode} {index} on:remove={removeColor} {showControls} />
+  </div>
+  {/each}
+</div>
