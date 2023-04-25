3a9405aad4cd34ab840e401a1a7dd765991f5256
Zooce <zooce@hey.com>
2023-04-24 22:59:19 -0700

    rm limit function + minor reformatting

--- src/lib/colorUtils.js
+++ src/lib/colorUtils.js
@@ -1,29 +1,25 @@
 /**
- * Limit `x
- * @param {number} x
- * @returns {number}
- */
-const L = (x) => Math.round(x * 1000) / 1000;
-
-/**
  * Convert RGB to HSV
  * @param {[number, number, number]} rgb - An array of RGB values [R, G, B] (0-255)
  * @returns {[number, number, number]} An array of HSV values [H, S, V] where H is in range [0, 360), S and V are in range [0, 100]
  */
 export function rgbToHsv(rgb) {
-  const r = L(rgb[0] / 255),
-        g = L(rgb[1] / 255),
-        b = L(rgb[2] / 255);
-  // const r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255;
-  const max = Math.max(r, g, b), min = Math.min(r, g, b);
+  const r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255;
+  const max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
   const diff = max - min;
-  const h = diff === 0 ? 0 :
+  let h = diff === 0 ? 0 :
     max === r ? (60 * ((g - b) / diff) + 360) % 360 :
     max === g ? (60 * ((b - r) / diff) + 120) :
     (60 * ((r - g) / diff) + 240);
-  const s = max === 0 ? 0 : diff / max;
-  const v = max;
-  return [Math.round(h), Math.round(s * 100), Math.round(v * 100)];
+  let s = max === 0 ? 0 : diff / max;
+  let v = max;
+  h = Math.round(h);
+  s = Math.round(s * 100);
+  v = Math.round(v * 100);
+  return [h, s, v];
 }
 
 /**
@@ -32,7 +28,9 @@ export function rgbToHsv(rgb) {
  * @returns {[number, number, number]} An array of RGB values [R, G, B] (0-255)
  */
 export function hsvToRgb(hsv) {
-  const h = hsv[0], s = hsv[1] / 100, v = hsv[2] / 100;
+  const h = hsv[0],
+        s = hsv[1] / 100,
+        v = hsv[2] / 100;
   const c = v * s;
   const hh = h / 60;
   const x = c * (1 - Math.abs(hh % 2 - 1));
@@ -50,7 +48,10 @@ export function hsvToRgb(hsv) {
     default: r = 0; g = 0; b = 0;
   }
 
-  return [Math.round((r + m) * 255), Math.round((g + m) * 255), Math.round((b + m) * 255)];
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+  return [r, g, b];
 }
 
 /**
